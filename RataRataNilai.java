a0a0c3668fd711acc807e190d05bd52d3f0cb7a8
Fatima X RPL 5 <fatima_pratista_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 16:44:32 +0700

    Add files via upload

---
+++ RataRataNilai.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class RataRataNilai {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = input.nextInt();
+        double totalNilai = 0;
+
+        for (int i = 1; i <= jumlahSiswa; i++) {
+            System.out.print("Masukkan nilai siswa ke- "+i+": ");
+            double nilai = input.nextDouble();
+            totalNilai += nilai;
+        }
+
+        double rataRata = totalNilai/jumlahSiswa;
+        System.out.println("Rata-rata nilai ujian Produktif RPL adalah: " + rataRata);
+    }
+}
